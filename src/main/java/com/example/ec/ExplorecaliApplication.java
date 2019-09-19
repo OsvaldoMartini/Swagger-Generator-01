98c9d00980f2c18e42200bbbd2d7adf0dec79d56
Osvaldo Martini <maos@corner.ch>
2019-09-19 17:26:33 +0200

    Updated Path Selector

--- src/main/java/com/example/ec/ExplorecaliApplication.java
+++ src/main/java/com/example/ec/ExplorecaliApplication.java
@@ -10,9 +10,13 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+
 import java.util.ArrayList;
 
 import static springfox.documentation.builders.PathSelectors.any;
+//import springfox.documentation.builders.PathSelectors;
+//import static springfox.documentation.builders.PathSelectors.regex;
+
 
 /**
  * Main Class for the Spring Boot Application
@@ -25,7 +29,8 @@ public class ExplorecaliApplication {
     @Bean
     public Docket docket() {
         return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.ec")).paths(any()).build()
+                //.apis(RequestHandlerSelectors.basePackage("com.example.ec")).paths(PathSelectors.regex("/.*")).build()
+				.apis(RequestHandlerSelectors.basePackage("com.example.ec")).paths(any()).build()
 				.forCodeGeneration(true)
                 .apiInfo(new ApiInfo("Explore Template API's", "API's for Template", "2.0", null,
 						new Contact("Osvaldo Martini", "http://localhost:9090", "maos@corner.ch"),
